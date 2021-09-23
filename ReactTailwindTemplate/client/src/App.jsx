5fc8be3ac54518ec5b48cdcc08eca6f4b7c54b10
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-09-23 12:20:29 -0500

    Add react tailwind template.

---
+++ ReactTailwindTemplate/client/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link "
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="text-red-500 text-4xl">Learn React</div>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
